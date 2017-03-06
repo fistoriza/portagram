cdccff8ecf7d06bf47b96a7fb9fe81ad69a37e1c
Fisto Riza Satianto <fistoriza@me.com>
2017-03-06 19:21:39 +0000

    styling the app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,8 +14,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:message)
   end
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize}", status: status
-  end
 end
