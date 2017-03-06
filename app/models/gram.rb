6c764c66ab2ae9af09add2e50d4e2ae0f4e311f7
Fisto Riza Satianto <fistoriza@me.com>
2017-03-06 17:54:52 +0000

    all tests done

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,8 @@
 class Gram < ApplicationRecord
-  belongs_to :user
-  mount_uploader :picture, PictureUploader
   validates :message, presence: true
+  validates :picture, presence: true
+
+  belongs_to :user
+  has_many :comments
+  mount_uploader :picture, PictureUploader
 end
